e346182aa78a70ba4e9ba2a7d741873ffd9abf49
Teddy <t.a.peterschmidt@gmail.com>
2025-02-10 17:12:42 -0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-primary w-screen h-screen">
+        <div className="bg-tertiary w-fit p-4 -rotate-20">
+          <h1 className="text-secondary font-header text-outline -rotate-5 ">
+            HANKSDAY.
+          </h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
