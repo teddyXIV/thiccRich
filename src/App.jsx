74922d3f7fe8ccc1d2934809f0cb2044535b8e51
Teddy <t.a.peterschmidt@gmail.com>
2025-02-11 18:41:14 -0800

    load fonts

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ function App() {
 
   return (
     <>
-      <div className="bg-primary w-screen h-screen">
-        <div className="bg-tertiary w-fit p-4 -rotate-20">
-          <h1 className="text-secondary font-header text-outline -rotate-5 ">
-            HANKSDAY.
-          </h1>
+      <div className="bg-primary w-screen h-screen relative">
+        <div className="bg-tertiary w-fit px-4 shadow-lg shadow-black border-2 absolute top-14 left-12 -rotate-2">
+          <p className="text-4xl font-patrick italic"><span className="font-bowlby italic text-secondary text-outline">O</span>PERATION</p>
         </div>
+        {/* <div className="bg-tertiary w-fit h-fit p-4 -rotate-12 absolute top-12 left-8"> */}
+        <p className="text-secondary font-bowlby text-outline italic -rotate-5 text-6xl absolute top-24 left-24">
+          HANKSDAY.
+        </p>
+        {/* </div> */}
       </div>
     </>
   )
