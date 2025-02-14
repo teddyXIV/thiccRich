0bdc0ff7d170a39d1f8b1916dcc41b310f000b8c
Teddy <t.a.peterschmidt@gmail.com>
2025-02-13 17:41:16 -0800

    add svgs

--- src/components/DesktopIcon.jsx
+++ src/components/DesktopIcon.jsx
@@ -1,5 +1,6 @@
 import { useDraggable } from "@neodrag/react";
 import { useRef } from "react";
+import icons from "../icons";
 
 const DesktopIcon = () => {
   const draggableRef = useRef(null);
@@ -10,9 +11,12 @@ const DesktopIcon = () => {
       ref={draggableRef}
       className="hover:bg-white/10 active:bg-white/25 w-24 p-2"
     >
+      {/* <img src={icons.baseball} /> */}
+      <img src={icons.surf} />
       <h1 className="text-3xl text-red-500">Drag me</h1>
     </div>
   )
 }
 
 export default DesktopIcon;
+
---
+++ src/icons.jsx
@@ -0,0 +1,27 @@
+import baseball from "./assets/baseball.svg";
+import beer from "./assets/beer.svg"
+import questionMark from "./assets/question-mark.svg";
+import heart from "./assets/heart.svg";
+import exclamation from "./assets/exclamation.svg";
+import fish from "./assets/fish.svg";
+import guitar from "./assets/guitar.svg";
+import poo from "./assets/poo.svg";
+import saxophone from "./assets/saxophone.svg";
+import ski from "./assets/ski.svg";
+import surf from "./assets/surf.svg";
+import trumpet from "./assets/trumpet.svg";
+
+export default {
+  baseball,
+  beer,
+  questionMark,
+  heart,
+  exclamation,
+  fish,
+  guitar,
+  poo,
+  saxophone,
+  ski,
+  surf,
+  trumpet
+}
